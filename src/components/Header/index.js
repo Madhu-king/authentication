8ea89507ee2d2e320c9efd427d3f4b403486ed1b
madhu <madhumolaka018@gmail.com>
2025-01-22 07:41:21 +0000

    updated

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,11 +3,24 @@ import './index.css'
 
 const Header = () => (
   <div className="navbar-container">
-    <img
-      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
-      className="logo-size-1"
-      alt="website logo"
-    />
+    <div className="align">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+        className="logo-size"
+        alt="website logo"
+      />
+      <div className="navbar-buttons-container">
+        <ul className="unorder-container">
+          <li className="btn-style">Home</li>
+          <li className="btn-style">Products</li>
+          <li className="btn-style">Cart</li>
+        </ul>
+        <button type="button" className="logout-style">
+          Logout
+        </button>
+      </div>
+    </div>
+
     <div>
       <div className="smalldevices-logo-container">
         <img
@@ -21,34 +34,23 @@ const Header = () => (
           alt="nav logoout"
         />
       </div>
-    </div>
-
-    <div className="navbar-buttons-container">
-      <ul className="unorder-container">
-        <li className="btn-style">Home</li>
-        <li className="btn-style">Products</li>
-        <li className="btn-style">Cart</li>
-      </ul>
-      <button type="button" className="logout-style">
-        Logout
-      </button>
-    </div>
-    <div className="navbar-images-container-1">
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
-        className="image-size"
-        alt="nav home"
-      />
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png "
-        className="image-size"
-        alt="nav products"
-      />
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
-        className="image-size"
-        alt="nav cart"
-      />
+      <div className="navbar-images-container-1">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
+          className="image-size"
+          alt="nav home"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png "
+          className="image-size"
+          alt="nav products"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
+          className="image-size"
+          alt="nav cart"
+        />
+      </div>
     </div>
   </div>
 )
